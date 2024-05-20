cbe613c0e8c5718b1fceda553686800178da6856
leonardo.beron <leonardoberon33@gmail.com>
2024-05-20 19:58:23 -0300

    if else

---
+++ tarea Condicionales/app.js
@@ -0,0 +1,108 @@
+/*
+/////  Tarea 1  /////
+
+let edad = prompt('Ingresa tu edad');
+
+if (edad < 0) {
+    console.log( "Edad no válida.")
+} else if (edad < 13) {
+    console.log('eres un niño')
+} else if ( edad >= 13 && edad <= 17  ) {
+    console.log("Eres un adolescente.")
+} else if (edad >= 18 && edad <= 64) {
+    console.log("Eres un adulto.")
+} else if (edad >= 65) {
+    console.log( "Eres una persona mayor.")
+}
+
+*/
+
+
+/*
+/////  Tarea 2  /////
+
+let calificacion = prompt('Ingresa una calificación numérica');
+
+if (calificacion >= 90 && calificacion <= 100){
+    console.log('A')
+} else if(calificacion >= 80 && calificacion <=89){
+    console.log('B')
+} else if(calificacion >= 70 && calificacion <=79) {
+    console.log('C')
+} else if (calificacion >= 60 && calificacion <= 69) {
+    console.log('B')
+} else if (calificacion >= 0 && calificacion <= 59) {
+    console.log('F')
+} else {
+    console.log("Calificación no válida.")
+}
+
+*/
+
+/*
+////  Tarea 3  ////
+
+let lado1 = prompt('Ingresa el primer Lado del triangulo')
+
+let lado2 = prompt('Ingresa el segundo Lado del triangulo')
+
+let lado3 = prompt('Ingresa el tercer Lado del triangulo')
+
+if (lado1 === lado2 && lado2 === lado3) {
+    console.log('Equilatero')
+} else if (lado1 === lado2 || lado1 === lado3 || lado2 === lado3) {
+    console.log('Isosceles')
+} else if(lado1 != lado2 && lado1 != lado3 && lado2 != lado3 ){
+    console.log('Escaleno')
+} else {
+    console.log('Longitudes no validas')
+}
+
+*/
+
+
+
+
+/*
+////  Tarea 4  ////
+
+let dia = prompt(' Introduzca un número del 1 al 7 ')
+
+if ( dia == 1) {
+    console.log('Lunes')
+} else if ( dia == 2) {
+    console.log('Martes')
+} else if( dia == 3) {
+    console.log('Miercoles')
+} else if ( dia == 4) {
+    console.log('Jueves')
+} else if( dia == 5) {
+    console.log('Viernes')
+} else if ( dia == 6) {
+    console.log('Sabado')
+} else if ( dia == 7) {
+    console.log('Domingo')
+} else {
+    console.log("Número no válido.")
+}
+
+*/
+
+
+/*
+//// tarea 5  ////
+
+let grados = prompt(' Introduzca una temperatura en grados Celsius')
+
+if ( grados <= 0) {
+    console.log('Sólido')
+} else if (grados >0 && grados <= 99) {
+    console.log('Liquido')
+} else if (grados >= 100) {
+    console.log('Gaseoso')
+} else {
+    console.log("Temperatura no válida.")
+}
+
+*/
+
